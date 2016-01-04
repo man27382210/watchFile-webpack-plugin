199ab073cb3baeea95047a9c4ab4479210544f8c
Davis <man27382210@gmail.com>
2016-01-05 00:51:38 +0800

    finish unit test

---
+++ index.js
@@ -0,0 +1,28 @@
+var watch = require('watch');
+var path  = require('path');
+var fs    = require('fs');
+
+
+function watchFilePlugin(options) {
+  this.options = options;
+}
+
+watchFilePlugin.prototype.apply = function(compiler) {
+  var that = this;
+  compiler.plugin("emit", function(compilation, callback) {
+    var watchFolder = path.join(__dirname, that.options.watchFolder);
+    watch.createMonitor(watchFolder, function(monitor){
+      monitor.files[path.join(watchFolder, "/*."+that.options.watchExtension)]
+      monitor.on("changed", function (f, curr, prev) {
+        console.log(f);
+        compiler.run(function(err) {
+          if(err) throw err;
+          monitor.stop();
+        });
+      });
+    });
+    callback();
+  });
+};
+
+module.exports = watchFilePlugin;
